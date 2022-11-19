7368c2b321f7f96f63654e051fcefc07fddfce85
naresh-webdev <gkmnaresh01@gmail.com>
2022-11-19 14:36:24 +0530

    continued after lunch

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("testing from app.ts");
+function app(n1, n2) {
+    return n1 + n2;
+}
+var number = 5;
+var number1;
+number1 = +prompt("enter a number");
+console.log("the entered number is : " + number1);
--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
 function app(n1: number, n2: number) {
   return n1 + n2;
 }
+
+const number = 5;
+let number1: number;
+
+number1 = +prompt("enter a number")!;
+
+console.log("the entered number is : " + number1);
