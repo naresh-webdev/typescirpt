68b18b12645ef1572a9d6b08f5bf82f6d38f63d1
naresh-webdev <gkmnaresh01@gmail.com>
2022-11-19 16:53:41 +0530

    enum skill aquired

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
+// // const person: {
+// //   name: string;
+// //   age: number;
+// // } = {
+// //   name: "Naresh",
+// //   age: 18,
+// // };
 // const person: {
 //   name: string;
 //   age: number;
+//   hobbies: string[];
+//   role: [number, string];
 // } = {
 //   name: "Naresh",
 //   age: 18,
+//   hobbies: ["programming", "anime"],
+//   role: [2, "author"],
 // };
+// person.role = [1, "Rimuru"];
+// let animes: string[];
+// animes = [
+//   "attack on titan",
+//   "erased",
+//   "that time I got reincarnated as a slime",
+// ];
+// console.log(person.role);
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Naresh",
     age: 18,
     hobbies: ["programming", "anime"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
-person.role = [1, "Rimuru"];
-var animes;
-animes = [
-    "attack on titan",
-    "erased",
-    "that time I got reincarnated as a slime",
-];
-console.log(person.role);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
--- app.ts
+++ app.ts
@@ -1,34 +1,51 @@
+// // const person: {
+// //   name: string;
+// //   age: number;
+// // } = {
+// //   name: "Naresh",
+// //   age: 18,
+// // };
+
 // const person: {
 //   name: string;
 //   age: number;
+//   hobbies: string[];
+//   role: [number, string];
 // } = {
 //   name: "Naresh",
 //   age: 18,
+//   hobbies: ["programming", "anime"],
+//   role: [2, "author"],
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// person.role = [1, "Rimuru"];
+
+// let animes: string[];
+// animes = [
+//   "attack on titan",
+//   "erased",
+//   "that time I got reincarnated as a slime",
+// ];
+
+// console.log(person.role);
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+enum Role {
+  ADMIN = 1, //default start at 0
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Naresh",
   age: 18,
   hobbies: ["programming", "anime"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-person.role = [1, "Rimuru"];
-
-let animes: string[];
-animes = [
-  "attack on titan",
-  "erased",
-  "that time I got reincarnated as a slime",
-];
-
-console.log(person.role);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
